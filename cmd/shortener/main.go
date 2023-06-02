e394465939b749b4396218973e1961c012aa95b9
Vlad Kvetkin <vlad.kvetkin@yandex.ru>
2023-06-02 22:45:26 +0300

    shortener http server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/VladKvetkin/shortener/internal/app/config"
+	"github.com/VladKvetkin/shortener/internal/app/handler"
+	"github.com/VladKvetkin/shortener/internal/app/router"
+	"github.com/VladKvetkin/shortener/internal/app/server"
+	"github.com/VladKvetkin/shortener/internal/app/storage"
+)
+
+func main() {
+	config := config.NewConfig("localhost", "8080")
+
+	storage := storage.NewStorage()
+
+	router := router.Router{
+		Routes: map[string]http.Handler{
+			"/": handler.MainHandler(storage),
+		},
+	}
+
+	server := server.NewServer(config, router)
+
+	err := server.Start()
+	if err != nil {
+		panic(err)
+	}
+}
